90de77746c95f8a3ba710fed6fe69748b50d1f32
aloongly <aloongly@gmail.com>
2017-07-22 11:50:34 +0800

    update huobi

--- bourse/huobi/huobi.go
+++ bourse/huobi/huobi.go
@@ -1,1 +1,71 @@
 package huobi
+
+import (
+	"fmt"
+
+	"github.com/Giantmen/trader/bourse/huobi/huobiN"
+	"github.com/Giantmen/trader/bourse/huobi/huobiO"
+	"github.com/Giantmen/trader/proto"
+)
+
+type Huobi struct {
+	huobiN *huobiN.Huobi
+	huobiO *huobiO.Huobi
+}
+
+func NewHuobi(accountid, accessKey, secretKey string, timeout int) (*Huobi, error) {
+	huobiN, err := huobiN.NewHuobi(accountid, accessKey, secretKey, timeout)
+	if err != nil {
+		return nil, err
+	}
+	huobiO, err := huobiO.NewHuobi(accountid, accessKey, secretKey, timeout)
+	if err != nil {
+		return nil, err
+	}
+	return &Huobi{
+		huobiN: huobiN,
+		huobiO: huobiO,
+	}, nil
+}
+
+func (huobi *Huobi) GetPriceOfDepth(size int, depth float64, currencyPair string) (*proto.Price, error) {
+	switch currencyPair {
+	case proto.BTC_CNY, proto.LTC_CNY:
+		return huobi.huobiO.GetPriceOfDepth(size, depth, currencyPair)
+	case proto.ETH_CNY, proto.ETC_CNY:
+		return huobi.huobiN.GetPriceOfDepth(size, depth, currencyPair)
+	default:
+		return nil, fmt.Errorf("currencyPair err %s", currencyPair)
+	}
+}
+
+func (huobi *Huobi) GetAccount() (*proto.Account, error) {
+
+	//huobi.huobiO.GetAccount(size, depth, currencyPair)
+	return huobi.huobiN.GetAccount()
+}
+
+func (huobi *Huobi) GetTicker(currencyPair string) (float64, error) {
+	return 0, nil
+}
+func (huobi *Huobi) Sell(amount, price, currencyPair string) (*proto.Order, error) {
+	return nil, nil
+}
+
+func (huobi *Huobi) Buy(amount, price, currencyPair string) (*proto.Order, error) {
+	switch currencyPair {
+	case proto.BTC_CNY, proto.LTC_CNY:
+		return huobi.huobiO.Buy(amount, price, currencyPair)
+	case proto.ETH_CNY, proto.ETC_CNY:
+		return huobi.huobiN.Buy(amount, price, currencyPair)
+	default:
+		return nil, fmt.Errorf("currencyPair err %s", currencyPair)
+	}
+}
+
+func (huobi *Huobi) CancelOrder(orderId, currencyPair string) (bool, error) {
+	return false, nil
+}
+func (huobi *Huobi) GetOneOrder(orderId, currencyPair string) (*proto.Order, error) {
+	return nil, nil
+}
--- bourse/huobi/huobiO/huobi.go
+++ bourse/huobi/huobiO/huobi.go
@@ -1,1 +1,42 @@
 package huobiO
+
+import "github.com/Giantmen/trader/proto"
+
+type Huobi struct {
+	accountid string
+	accessKey string
+	secretKey string
+	timeout   int
+}
+
+func NewHuobi(accountid, accessKey, secretKey string, timeout int) (*Huobi, error) {
+	return &Huobi{
+		accountid: accountid,
+		accessKey: accessKey,
+		secretKey: secretKey,
+		timeout:   timeout,
+	}, nil
+}
+
+func (huobi *Huobi) GetPriceOfDepth(size int, depth float64, currencyPair string) (*proto.Price, error) {
+	return nil, nil
+}
+func (huobi *Huobi) GetAccount() (*proto.Account, error) {
+	return nil, nil
+}
+
+func (huobi *Huobi) GetTicker(currencyPair string) (float64, error) {
+	return 0, nil
+}
+func (huobi *Huobi) Sell(amount, price, currencyPair string) (*proto.Order, error) {
+	return nil, nil
+}
+func (huobi *Huobi) Buy(amount, price, currencyPair string) (*proto.Order, error) {
+	return nil, nil
+}
+func (huobi *Huobi) CancelOrder(orderId, currencyPair string) (bool, error) {
+	return false, nil
+}
+func (huobi *Huobi) GetOneOrder(orderId, currencyPair string) (*proto.Order, error) {
+	return nil, nil
+}
